f206104208cded7ff6386a14ea917415d79cad9c
James Carlo Luchavez <mrstudyfirst@gmail.com>
2021-10-06 15:19:54 +0800

    Reduced queue workers to 2

--- .platform/hooks/postdeploy/01_queue.sh
+++ .platform/hooks/postdeploy/01_queue.sh
@@ -2,5 +2,5 @@
 cp /opt/elasticbeanstalk/deployment/env /opt/elasticbeanstalk/deployment/laravel_env
 sudo chmod 644 /opt/elasticbeanstalk/deployment/laravel_env
 rm -f /opt/elasticbeanstalk/deployment/*.bak
-sudo systemctl enable laravel_queue_worker@{1..8}.service
-sudo systemctl restart laravel_queue_worker@{1..8}.service
+sudo systemctl enable laravel_queue_worker@{1..2}.service
+sudo systemctl restart laravel_queue_worker@{1..2}.service
